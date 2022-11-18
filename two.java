6e70e3f4e6ca2a9f6a9e53ceace3f615b9f1d626
Laxmigit1 <118277581+Laxmigit1@users.noreply.github.com>
2022-11-18 13:17:25 +0000

    Add files via upload

--- two.java
+++ two.java
@@ -1,22 +1,9 @@
-class Main {
-  public static void main(String[] args) {
+programs in different Categories.
 
-    // create a file object for the current location
-    File file = new File("newFile.txt");
-
-    try {
-
-      // trying to create a file based on the object
-      boolean value = file.createNewFile();
-      if (value) {
-        System.out.println("The new file is created.");
-      }
-      else {
-        System.out.println("The file already exists.");
-      }
-    }
-    catch(Exception e) {
-      e.getStackTrace();
-    }
-  }
+class HelloWorld 1
+{
+   public static void main(String args[])
+   {
+      System.out.println("Hello World 1");
+   }
 }
